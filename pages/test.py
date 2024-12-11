7dc339a22c985b15d82dca98e3cf00a615241e71
liuchia515 <tinaliu0515@gmail.com>
2024-12-11 16:12:41 +0800

    Create test.py

---
+++ pages/test.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+import pydeck as pdk
+import pandas as pd
+import numpy as np
+
+st.title("3D觀測值分布圖")
+url="https://raw.githubusercontent.com/liuchia515/gisappreport/refs/heads/main/data/%E8%A7%80%E6%B8%AC%E5%80%BC.csv"
+data = pd.read_csv(
+    url,
+    header=0,
+    names=[
+      "測站名稱",
+      "震度值",
+      "震央距(Km)",
+      "垂直向(gal)",
+      "南北向(gal)",
+      "東西向(gal)",
+      "縣市",
+      "鄉鎮",
+      "lat",
+      "lon",
+    ],
+)
+
+options = st.multiselect("選擇多個選項", data.columns)
+st.write("您選擇了：", options)
+
+st.pydeck_chart(
+    pdk.Deck(
+        initial_view_state=pdk.ViewState(
+            latitude=23.5,
+            longitude=121,
+            zoom=7,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=data,
+                get_position="[lon, lat]",
+                get_radius="震度值",
+                auto_highlight=True,
+                elevation_scale=50,
+                pickable=True,
+                extruded=True,
+            ),
+        ],
+    )
+)
+
+
+st.markdown("測站資料表格")
+df = pd.read_csv(url)
+st.dataframe(df)

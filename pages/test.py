33d6890b88843778c0f24e3fa064ecacb2d1d75d
liuchia515 <tinaliu0515@gmail.com>
2024-12-13 17:59:40 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -5,3 +5,23 @@ import pandas as pd
 import numpy as np
 
 st.title("測試用頁面")
+url="https://raw.githubusercontent.com/liuchia515/gisappreport/refs/heads/main/data/%E6%AD%B7%E5%8F%B2%E8%B3%87%E6%96%99.csv"
+data = pd.read_csv(url)
+
+selected= st.slider("請依照需求自行調整範圍",5.0,7.3,(5.0,7.3))
+def filterdata(df,selected_range):
+  lower, upper = selected_range
+  return df[(df["ML"]>=lower) & (df["ML"]<=upper)]
+filtered_data = filterdata(data, selected)
+m=leafmap.Map(center=[23.5, 121], zoom=7)
+m.add_points_from_xy(
+  filtered_data,
+  x="lon",
+  y="lat",
+  color_column="region",
+  icon_names=["gear", "map", "leaf", "globe"],
+  spin=True,
+  add_legend=True,
+)
+
+st.dataframe(filtered_data)

cda0c18e56a28eb5cd8e266ff48444f6fc725e44
liuchia515 <tinaliu0515@gmail.com>
2024-12-16 18:43:42 +0800

    Update  1.environment.py

--- pages/ 1.environment.py
+++ pages/ 1.environment.py
@@ -6,21 +6,7 @@ import numpy as np
 
 st.title("3D觀測值分布圖")
 url="https://raw.githubusercontent.com/liuchia515/gisappreport/refs/heads/main/data/%E8%A7%80%E6%B8%AC%E5%80%BC.csv"
-data = pd.read_csv(
-    url,
-    header=0,
-    names=[
-      "測站名稱",
-      "震度值",
-      "震央距(Km)",
-      "垂直向(gal)",
-      "南北向(gal)",
-      "東西向(gal)",
-      "鄉鎮",
-      "lat",
-      "lon",
-    ],
-)
+data = pd.read_csv(url)
 col1,col2=st.columns([2,1])
 width = None
 height = 800

4c7131686fc5f4f6e5b575ba338b3fdce451c931
b6baa <tina200205106@gmail.com>
2024-12-16 12:47:00 +0800

    Update  3. Socioeconomic Data.py

--- pages/ 3. Socioeconomic Data.py
+++ pages/ 3. Socioeconomic Data.py
@@ -18,7 +18,7 @@ gdf = gdf[gdf['COUNTYNAME'] == '臺南市']
 
 hospital_csv = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%86%AB%E7%99%82%E9%99%A2%E6%89%80%E6%95%B8%E9%87%8F/104%E5%B9%B412%E6%9C%88%E8%A1%8C%E6%94%BF%E5%8D%80%E9%86%AB%E7%99%82%E9%99%A2%E6%89%80%E7%B5%B1%E8%A8%88_%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80_%E8%87%BA%E5%8D%97%E5%B8%82.csv'
 hospital_data = pd.read_csv(hospital_csv)
-hospital_data = hospital_data[[TOWN, H_CNT, H_BED, H_SRVP, H_SRVB]]
+hospital_data = hospital_data[['TOWN', 'H_CNT', 'H_BED', 'H_SRVP', 'H_SRVB']]
 hospital_data = hospital_data.rename(columns={"TOWN": "鄉鎮市區名稱", 'H_CNT': '醫療院所家數', 'H_BED': '醫療院所床數', 'H_SRVP': '醫療院所平均每家服務人數', 'H_SRVB': '醫療院所平均每千人擁有病床數'})
 
 gdf = gdf.merge(hospital_data, left_on='TOWNNAME', right_on='TOWN', how='left')

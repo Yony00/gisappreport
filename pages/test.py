6a59b122e4f7f02cd6242a37eae1ebcb396a9780
liuchia515 <tinaliu0515@gmail.com>
2024-12-16 17:05:09 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -8,7 +8,7 @@ import leafmap.foliumap as leafmap
 import folium
 
 st.title("測試用頁面")
-m = leafmap.Map(center=[23.5, 121], zoom=7)
+m = leafmap.Map(center=[23.15, 120.2], zoom=9)
 polygon = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80%E7%95%8C/%E9%84%89(%E9%8E%AE%E3%80%81%E5%B8%82%E3%80%81%E5%8D%80)%E7%95%8C%E7%B7%9A(TWD97%E7%B6%93%E7%B7%AF%E5%BA%A6)1131028/TOWN_MOI_1131028.shp'
 gdf = gpd.read_file(polygon)
 gdf = gdf[gdf['COUNTYNAME'] == '臺南市']

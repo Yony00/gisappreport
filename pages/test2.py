98ea16139ec27d4d17f996fa30aed3b6fec5c3e9
liuchia515 <tinaliu0515@gmail.com>
2024-12-16 19:20:54 +0800

    Update test2.py

--- pages/test2.py
+++ pages/test2.py
@@ -7,7 +7,7 @@ st.title("3D觀測值分布圖")
 url="https://raw.githubusercontent.com/liuchia515/gisappreport/refs/heads/main/data/%E8%A7%80%E6%B8%AC%E5%80%BC.csv"
 data = pd.read_csv(url)
 
-data['color'] = data['震度值'].apply(lambda x: [255, 255 - x * 28, 10+x*25])  # 根據震度值設定顏色
+data['color'] = data['震度值'].apply(lambda x: [255, 255 - x * 28, 10 + x * 25])  # 根據震度值設定顏色
 data['elevation'] = data['震度值'] * 100  # 根據震度值設定高度
 data['radius'] = data['震央距(Km)']*30
 required_columns = ['lat', 'lon', '震度值', '震央距(Km)', 'color', 'elevation', 'radius']
@@ -19,17 +19,17 @@ if all(col in data.columns for col in required_columns):
         get_position='[lon, lat]',  # 經緯度位置
         get_radius='radius',  # 根據震央距(Km)設定半徑
         get_fill_color='color',  # 使用預處理的顏色欄位
-        extruded=True,
         get_elevation='elevation',  # 使用預處理的高度欄位
         auto_highlight=True,  # 高亮選中點
+        extruded=True,
     )
 
     # 設定地圖視角
     view_state = pdk.ViewState(
-        latitude=data['lat'].mean(),  # 設定地圖中心的緯度
-        longitude=data['lon'].mean(),  # 設定地圖中心的經度
-        zoom=10,  # 地圖縮放級別
-        pitch=45,  # 地圖傾斜角度
+        latitude=23.15,  # 設定地圖中心的緯度
+        longitude=120.3,  # 設定地圖中心的經度
+        zoom=9,  # 地圖縮放級別
+        pitch=50,  # 地圖傾斜角度
         bearing=0  # 地圖旋轉角度
     )
 

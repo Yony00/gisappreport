7074fcbbc7cb83f22b53ad675614166eb7dc0988
liuchia515 <tinaliu0515@gmail.com>
2024-12-16 19:44:58 +0800

    Update  1.environment_ver2.py

--- pages/ 1.environment_ver2.py
+++ pages/ 1.environment_ver2.py
@@ -13,29 +13,63 @@ tiles = None
 
 data['color'] = data['震度值'].apply(lambda x: [255, 255 - x * 30, 10 + x * 25])
 data['radius'] = data['震度值']*250
-scatterplot_layer = pdk.Layer(
-    'ScatterplotLayer',
-    data=data,  # 資料來源
-    get_position='[lon, lat]',  # 經緯度位置
-    get_radius='radius',  # 根據震央距(Km)設定半徑
-    get_fill_color='color',  # 使用預處理的顏色欄位
-    auto_highlight=True,  # 高亮選中點
-    pickable=True,
-)
-view_state = pdk.ViewState(
-    latitude=23.15,  # 設定地圖中心的緯度
-    longitude=120.3,  # 設定地圖中心的經度
-    zoom=9,  # 地圖縮放級別
-    pitch=50,  # 地圖傾斜角度
-    bearing=0  # 地圖旋轉角度
-)
-deck = pdk.Deck(
-    layers=[scatterplot_layer],
-    initial_view_state=view_state,
-    tooltip={"text": "測站名稱: {測站名稱}\n震度: {震度值}"},
-)
-st.pydeck_chart(deck,on_select="rerun")
 
-st.markdown("所有測站資料表格")
-df = pd.read_csv(url)
-st.dataframe(df)
+with col1:
+    optiona = data["鄉鎮"].unique().tolist()
+    optionb = st.multiselect("選擇行政區（多選）", optiona)
+    if optionb:
+        filtered = data[data["鄉鎮"].isin(optionb)]
+        scatterplot_layer = pdk.Layer(
+            'ScatterplotLayer',
+            data=filtered,
+            get_position='[lon, lat]',
+            get_radius='radius',
+            get_fill_color='color', 
+            auto_highlight=True,
+            pickable=True,
+        )
+        view_state = pdk.ViewState(
+            latitude=23.15,
+            longitude=120.3,
+            zoom=9,
+            pitch=50,
+            bearing=0,
+        )
+        deck = pdk.Deck(
+            layers=[scatterplot_layer],
+            initial_view_state=view_state,
+            tooltip={"text": "測站名稱: {測站名稱}\n震度: {震度值}"},
+        )
+        st.pydeck_chart(deck,on_select="rerun")
+    else:
+        scatterplot_layer = pdk.Layer(
+            'ScatterplotLayer',
+            data=data,
+            get_position='[lon, lat]',
+            get_radius='radius',
+            get_fill_color='color', 
+            auto_highlight=True,
+            pickable=True,
+        )
+        view_state = pdk.ViewState(
+            latitude=23.15,
+            longitude=120.3,
+            zoom=9,
+            pitch=50,
+            bearing=0,
+        )
+        deck = pdk.Deck(
+            layers=[scatterplot_layer],
+            initial_view_state=view_state,
+            tooltip={"text": "測站名稱: {測站名稱}\n震度: {震度值}"},
+        )
+        st.pydeck_chart(deck,on_select="rerun")
+
+with col2:
+    if optionb:
+        st.markdown("選取區資料表格")
+        st.dataframe(filtered) 
+    else:
+        st.markdown("所有測站資料表格")
+        df = pd.read_csv(url)
+        st.dataframe(df)    

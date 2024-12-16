dc4cb365ce61be56e12665595bfd26a21a50ba0f
liuchia515 <tinaliu0515@gmail.com>
2024-12-16 18:57:34 +0800

    Update test2.py

--- pages/test2.py
+++ pages/test2.py
@@ -7,21 +7,6 @@ import numpy as np
 st.title("3D觀測值分布圖")
 url="https://raw.githubusercontent.com/liuchia515/gisappreport/refs/heads/main/data/%E8%A7%80%E6%B8%AC%E5%80%BC.csv"
 data = pd.read_csv(url)
-col1,col2=st.columns([2,1])
-width = None
-height = 800
-tiles = None
-
-def get_color(震度值):
-    if 震度值 == 4:
-        return [255, 255, 204,255]  # 淡黃色
-    elif 震度值 == 5:
-        return [255, 255, 0,255]    # 黃色
-    elif 震度值 == 6:
-        return [255, 153, 51,255]   # 橙色
-    else:
-        return [255, 0, 0,255]      # 紅色
-data['color'] = data['震度值'].apply(get_color)
 
 st.pydeck_chart(
     pdk.Deck(
@@ -34,7 +19,7 @@ st.pydeck_chart(
         layers=[
             pdk.Layer(
                 "ScatterplotLayer",
-                data,
+                data=data,
                 get_position="[lon, lat]",
                 get_radius="震度值",
                 auto_highlight=True,

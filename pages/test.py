8b7646c2c43a7562ce594ab6105af5fa8c3504bb
liuchia515 <tinaliu0515@gmail.com>
2024-12-16 18:11:58 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -28,3 +28,9 @@ hosdata=pd.read_csv(hosdata_url)
 st.dataframe(hosdata)
 
 
+#獨居老人數、中低收入戶人數
+st.subheader("獨居老人數、中低收入戶人數")
+old_url="https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E7%8D%A8%E5%B1%85%E8%80%81%E4%BA%BA%E4%BA%BA%E6%95%B8/104%E5%B9%B4%E8%A1%8C%E6%94%BF%E5%8D%80%E5%B9%B4%E5%BA%95%E5%88%97%E5%86%8A%E9%9C%80%E9%97%9C%E6%87%B7%E4%B9%8B%E7%8D%A8%E5%B1%85%E8%80%81%E4%BA%BA%E4%BA%BA%E6%95%B8%E7%B5%B1%E8%A8%88_%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80_%E8%87%BA%E5%8D%97%E5%B8%82.csv"
+low_url="https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E4%B8%AD%E4%BD%8E%E6%94%B6%E5%85%A5%E6%88%B6%E7%B5%B1%E8%A8%88/104%E5%B9%B412%E6%9C%88%E8%A1%8C%E6%94%BF%E5%8D%80%E4%B8%AD%E4%BD%8E%E6%94%B6%E5%85%A5%E6%88%B6%E7%B5%B1%E8%A8%88%E6%8C%87%E6%A8%99_%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80_%E8%87%BA%E5%8D%97%E5%B8%82.csv"
+
+

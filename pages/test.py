62ecea97d087565387978d5d51fd887ccd52bfdb
liuchia515 <tinaliu0515@gmail.com>
2024-12-16 17:33:50 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -2,4 +2,12 @@ import streamlit as st
 import pydeck as pdk
 import pandas as pd
 import numpy as np
+import leafmap.foliumap as leafmap
 st.title("測試用頁面")
+
+#臺南市醫院資訊
+hos_url="https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%86%AB%E7%99%82%E9%99%A2%E6%89%80%E6%95%B8%E9%87%8F/%E8%87%BA%E5%8D%97%E5%B8%82%E9%86%AB%E7%99%82%E9%99%A2%E6%89%80%E9%BB%9E%E4%BD%8D%E8%B3%87%E6%96%99.csv"
+hos = pd.read_csv(hos_url)
+st.map(hos, size=20, color="#0044ff")
+
+st.dataframe(hos)

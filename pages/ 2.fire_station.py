968f457a5c82b73b4f1c43b4180cccba136938e1
tim9810 <tesla9810@gmail.com>
2024-12-17 15:37:28 +0800

    Update  2.fire_station.py

--- pages/ 2.fire_station.py
+++ pages/ 2.fire_station.py
@@ -17,9 +17,9 @@ tainan = taiwan[taiwan['COUNTYNAME'] == '臺南市']
 firestation_point_csv = 'https://raw.githubusercontent.com/tim9810/gis_final_exam/refs/heads/main/%E5%8F%B0%E5%8D%97%E6%B6%88%E9%98%B2%E5%B1%80wgs84%E5%BA%A7%E6%A8%99utf.csv'
 firestation_point = pd.read_csv(firestation_point_csv)
 
-option_list = hospital_point["行政區"].unique().tolist()
+option_list = firestation_point["行政區"].unique().tolist()
 option = st.multiselect("選擇行政區", option_list)
-filtered = hospital_point[hospital_point["行政區"].isin(option)]
+filtered = firestation_point[firestation_point["行政區"].isin(option)]
 
 m = leafmap.Map(center=[23, 120.3], zoom=10)
 m.add_points_from_xy(

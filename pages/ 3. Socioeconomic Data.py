5fc14599dc7d0e006f25082bb5f211a355590328
b6baa <tina200205106@gmail.com>
2024-12-17 10:49:33 +0800

    Update  3. Socioeconomic Data.py

--- pages/ 3. Socioeconomic Data.py
+++ pages/ 3. Socioeconomic Data.py
@@ -7,35 +7,31 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 st.title("臺南市社會經濟資料")
-st.header("醫療院所資料統計")
+st.header("臺南市各醫院位置")
 markdown = "（內容）"
 st.markdown(markdown)
 
 m = leafmap.Map(center=[23.5, 121], zoom=7)
 
-polygon = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80%E7%95%8C/%E9%84%89(%E9%8E%AE%E3%80%81%E5%B8%82%E3%80%81%E5%8D%80)%E7%95%8C%E7%B7%9A(TWD97%E7%B6%93%E7%B7%AF%E5%BA%A6)1131028/TOWN_MOI_1131028.shp'
-gdf = gpd.read_file(polygon)
-gdf = gdf[gdf['COUNTYNAME'] == '臺南市']
+#polygon = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80%E7%95%8C/%E9%84%89(%E9%8E%AE%E3%80%81%E5%B8%82%E3%80%81%E5%8D%80)%E7%95%8C%E7%B7%9A(TWD97%E7%B6%93%E7%B7%AF%E5%BA%A6)1131028/TOWN_MOI_1131028.shp'
+#gdf = gpd.read_file(polygon)
+#gdf = gdf[gdf['COUNTYNAME'] == '臺南市']
 
-hospital_csv = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%86%AB%E7%99%82%E9%99%A2%E6%89%80%E6%95%B8%E9%87%8F/104%E5%B9%B412%E6%9C%88%E8%A1%8C%E6%94%BF%E5%8D%80%E9%86%AB%E7%99%82%E9%99%A2%E6%89%80%E7%B5%B1%E8%A8%88_%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80_%E8%87%BA%E5%8D%97%E5%B8%82.csv'
-hospital_data = pd.read_csv(hospital_csv)
-hospital_data = hospital_data[['TOWN', 'H_CNT', 'H_BED', 'H_SRVP', 'H_SRVB']]
-hospital_data = hospital_data.rename(columns={"TOWN": "鄉鎮市區名稱", 'H_CNT': '醫療院所家數', 'H_BED': '醫療院所床數', 'H_SRVP': '醫療院所平均每家服務人數', 'H_SRVB': '醫療院所平均每千人擁有病床數'})
+hospital_point_csv = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E8%87%BA%E5%8D%97%E5%B8%82%E9%86%AB%E7%99%82%E9%99%A2%E6%89%80%E9%BB%9E%E4%BD%8D%E8%B3%87%E6%96%99.csv'
+hospital_point = pd.read_csv(hospital_point_csv)
 
-gdf = gdf.merge(hospital_data, left_on='TOWNNAME', right_on='鄉鎮市區名稱', how='left')
-gdf['醫療院所家數'] = pd.to_numeric(gdf['醫療院所家數'], errors='coerce').fillna(0) 
-
-m.add_gdf(
-    gdf,
-    layer_name="醫療院所家數",
-    color_column="醫療院所家數",
-    cmap="Blues",
-    style={"fillOpacity": 0.7, "weight": 1},
-    info_mode="on_hover", 
-)
-
+m.add_points_from_xy(
+            hospital_point,
+            x='經度',
+            y='緯度',
+            popup=["地址"],
+            icon_names=["red cross"],
+            spin=True,
+            add_legend=True,
+            layer_name="醫院點位",
+        )
 m.to_streamlit(height=700)
 
-st.header("醫療院所資料統計")
+st.header("各行政區救護車數量")
 markdown = "（內容）"
 st.markdown(markdown)

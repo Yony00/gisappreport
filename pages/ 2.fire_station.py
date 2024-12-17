a77df2b69cea7a10c5d3cf8850cc8060fdc3ba61
tim9810 <tesla9810@gmail.com>
2024-12-17 15:25:30 +0800

    Update  2.fire_station.py

--- pages/ 2.fire_station.py
+++ pages/ 2.fire_station.py
@@ -7,9 +7,10 @@ import pandas as pd
 st.set_page_config(layout="wide")
 st.title("臺南市各消防局統計資料")
 st.header("臺南市各消防局點位")
+markdown = "（內容）"
 st.markdown(markdown)
 
-polygon = 'https://github.com/.../TOWN_MOI_1131028.shp'
+polygon = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80%E7%95%8C/%E9%84%89(%E9%8E%AE%E3%80%81%E5%B8%82%E3%80%81%E5%8D%80)%E7%95%8C%E7%B7%9A(TWD97%E7%B6%93%E7%B7%AF%E5%BA%A6)1131028/TOWN_MOI_1131028.shp'
 taiwan = gpd.read_file(polygon)
 tainan = taiwan[taiwan['COUNTYNAME'] == '臺南市']
 

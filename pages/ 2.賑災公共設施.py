4298158ca3b9f280a0d49dcc6be4ecbfe30e36ad
tim9810 <tesla9810@gmail.com>
2024-12-17 19:30:38 +0800

    Update  2.賑災公共設施.py

--- pages/ 2.賑災公共設施.py
+++ pages/ 2.賑災公共設施.py
@@ -30,7 +30,7 @@ m.add_points_from_xy(
     popup=['地址','行政區'],
     layer_name="消防局點位",
 )
-m.to_streamlit(height=400)
+m.to_streamlit(height=500)
 
 if option:
     st.markdown("### 選取的行政區消防局資料")
@@ -66,7 +66,7 @@ m1.add_points_from_xy(
     popup1=['收容所名稱','地址','行政區','最大容納人數'],
     layer_name1="避難所點位",
 )
-m1.to_streamlit(height=400)
+m1.to_streamlit(height=500)
 
 if option1:
     st.markdown("### 選取的行政區避難所資料")
@@ -89,12 +89,6 @@ m2.add_heatmap(
             name='熱區圖',
             radius=20,
         )
-# 添加圖例
-m2.add_colorbar(
-    colors=["blue", "green", "yellow", "red"],  # 熱區顏色範圍
-    vmin=refuge_point["最大容納人數"].min(),
-    vmax=refuge_point["最大容納人數"].max(),
-    caption="最大容納人數"
-)
 
-m2.to_streamlit(height=400)
+
+m2.to_streamlit(height=500)

d54fb4d783f0d916ae12fcc0d7a2c707eeb4d7b5
b6baa <tina200205106@gmail.com>
2024-12-17 11:13:26 +0800

    Update  3. Socioeconomic Data.py

--- pages/ 3. Socioeconomic Data.py
+++ pages/ 3. Socioeconomic Data.py
@@ -3,20 +3,6 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 import geopandas as gpd
 import pandas as pd
-from matplotlib import font_manager
-import os
-import requests
-
-font_url = "https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/jf-openhuninn-2.0.ttf"
-font_path = "jf-openhuninn-2.0.ttf"
-
-if not os.path.isfile(font_path):
-    with open(font_path, "wb") as f:
-        f.write(requests.get(font_url).content)
-
-prop = font_manager.FontProperties(fname=font_path)
-plt.rcParams['font.family'] = prop.get_name()
-plt.rcParams['axes.unicode_minus'] = False
 
 st.set_page_config(layout="wide")
 
@@ -25,7 +11,7 @@ st.header("臺南市各醫院位置")
 markdown = "（內容）"
 st.markdown(markdown)
 
-m = leafmap.Map(center=[23.3, 120.3], zoom=10)
+m = leafmap.Map(center=[23.2, 120.3], zoom=10)
 
 polygon = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80%E7%95%8C/%E9%84%89(%E9%8E%AE%E3%80%81%E5%B8%82%E3%80%81%E5%8D%80)%E7%95%8C%E7%B7%9A(TWD97%E7%B6%93%E7%B7%AF%E5%BA%A6)1131028/TOWN_MOI_1131028.shp'
 taiwan = gpd.read_file(polygon)
@@ -58,6 +44,5 @@ tainan['count'] = tainan['count'].fillna(0)
 
 fig, ax = plt.subplots(figsize = (10, 6))
 tainan.plot(column = 'count',cmap='OrRd', ax = ax, legend=True)
-plt.title('各行政區救護車數量')
 plt.axis('off')
 st.pyplot(fig)

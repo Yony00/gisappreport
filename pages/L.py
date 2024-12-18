b201c7ef0a05b9bd2aabab734d239c8a002ca6b4
liuchia515 <tinaliu0515@gmail.com>
2024-12-18 16:47:19 +0800

    Update L.py

--- pages/L.py
+++ pages/L.py
@@ -1,5 +1,5 @@
 import streamlit as st
-lat = st.number_input("請填入經度",value=None,min_value=119.000,max_value=121.000)
-lon = st.number_input("請填入緯度",value=None,min_value=20.000,max_value=23.000)
+lat = st.number_input("請填入經度",value=None,min_value=119.990,max_value=120.750)
+lon = st.number_input("請填入緯度",value=None,min_value=22.850,max_value=23.450)
 
 st.write("經度:", lon,",緯度:",lat)

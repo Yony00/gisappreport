1bb7c13c5cb3235ff90207eaa9389f841eca113e
liuchia515 <tinaliu0515@gmail.com>
2024-12-18 16:40:38 +0800

    Update L.py

--- pages/L.py
+++ pages/L.py
@@ -1,5 +1,5 @@
 import streamlit as st
-lat = st.number_input("lat",value=float,min_value=20,max_value=23)
-lon = st.number_input("lat",value=float,min_value=119,max_value=121)
+lat = st.number_input("lat",value=None,min_value=20.000,max_value=23.000)
+lon = st.number_input("lat",value=None,min_value=119.000,max_value=121.000)
 
 st.write("lat&lon:", lat,",",lon)

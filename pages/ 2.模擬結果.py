c388bafdec2c8055f1d46cf75b91552663ffe995
liuchia515 <tinaliu0515@gmail.com>
2024-12-18 14:21:13 +0800

    Update  2.模擬結果.py

--- pages/ 2.模擬結果.py
+++ pages/ 2.模擬結果.py
@@ -21,36 +21,8 @@ data = pd.read_csv(
     ],
 )
 
-selectable_columns = ['請選擇一種GMPE','邏輯樹', 'AbrahamsonEtAl2014','BooreAtkinson2008','CampbellBozorgnia2008','ChiouYoungs2008','LinLee2008SInter']
+selectable_columns = ['邏輯樹', 'AbrahamsonEtAl2014','BooreAtkinson2008','CampbellBozorgnia2008','ChiouYoungs2008','LinLee2008SInter']
 options = st.selectbox('選擇一種GMPE呈現', selectable_columns)
-if options=="請選擇一種GMPE":
-    st.write("目前顯示為觀測值熱圖")
-    url2="https://raw.githubusercontent.com/liuchia515/gisappreport/refs/heads/main/data/%E8%A7%80%E6%B8%AC%E5%80%BC.csv"
-    data2 = pd.read_csv(url2)
-    st.pydeck_chart(
-        pdk.Deck(
-            initial_view_state=pdk.ViewState(
-                latitude=23.15,
-                longitude=120.3,
-                zoom=9,
-                pitch=30,
-            ),
-            layers=[
-                pdk.Layer(
-                    "HeatmapLayer",
-                    data=data2,
-                    get_position="[lon, lat]",
-                    get_weight="震度值",
-                    auto_highlight=True,
-                    radius_pixels=100,
-                    pickable=True,
-                    extruded=True,
-                    opacity=0.5
-                ),
-            ],
-        )
-    )
-
 if options=="邏輯樹":
     st.pydeck_chart(
         pdk.Deck(

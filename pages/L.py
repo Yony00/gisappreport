d0d1980322880201971cbd0119523979fd201800
liuchia515 <tinaliu0515@gmail.com>
2024-12-18 16:35:06 +0800

    Update L.py

--- pages/L.py
+++ pages/L.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 with st.popover("Open popover"):
-    lat,lon = st.text_input("lat","lon")
+    lat = st.text_input("lat")
+    lon = st.text_input(""lon")
 
 st.write("lat&lon:", lat,lon)

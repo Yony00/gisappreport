488e61e3bb3a7ef69d4b84bd164f4a3ba413a6ba
b6baa <tina200205106@gmail.com>
2024-12-18 15:50:04 +0800

    Update 5.尋找10公里內醫療與避難設施.py

--- pages/5.尋找10公里內醫療與避難設施.py
+++ pages/5.尋找10公里內醫療與避難設施.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+import geopandas as gpd
+import pandas as pd
+import folium
+from matplotlib import rcParams
+from matplotlib.font_manager import FontProperties
 
+font_path = "data/jf-openhuninn-2.0.ttf"
+font = FontProperties(fname = font_path)
+rcParams['font.family'] = font.get_name()
+
+st.set_page_config(layout="wide")

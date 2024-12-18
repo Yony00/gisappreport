30b42a4ac5364e37b7044a007c5e7e4338196261
liuchia515 <tinaliu0515@gmail.com>
2024-12-18 13:18:21 +0800

    Update  1.環境介紹.py

--- pages/ 1.環境介紹.py
+++ pages/ 1.環境介紹.py
@@ -11,7 +11,7 @@ width = None
 height = 800
 tiles = None
 
-data['color'] = data['震度值'].apply(lambda x: [255, 255 - (x*x*x), x*x*x])
+data['color'] = data['震度值'].apply(lambda x: [255, 255 - (x*x*5), x*x*x])
 data['radius'] = data['震度值'].apply(lambda x:x*x*x*10)
 
 with col1:

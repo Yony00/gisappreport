bbe233f4cbed66fe23f5eea1a613aa2b8cdb2266
b6baa <tina200205106@gmail.com>
2024-12-18 13:45:10 +0800

    Update  3. 醫療資源.py

--- pages/ 3. 醫療資源.py
+++ pages/ 3. 醫療資源.py
@@ -77,7 +77,7 @@ if option_2:
         count_op = tainan.loc[tainan['TOWNNAME'] == selected, 'count'].values[0]
         st.markdown(f"{selected} 共有救護車 {int(count_op)} 台。")
 
-fig, ax = plt.subplots(figsize = (10, 6))
+fig, ax = plt.subplots(figsize = (6, 6))
 tainan.plot(column = 'count',cmap='OrRd', ax = ax, legend=True)
 ax.set_title('各行政區救護車數量面量圖', fontproperties=font, fontsize=16)
 plt.axis('off')

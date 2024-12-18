c6751be1c0f4a2b77197c8dd96712e90e1c40ea9
b6baa <tina200205106@gmail.com>
2024-12-18 15:52:38 +0800

    Update 5.尋找10公里內醫療與避難設施.py

--- pages/5.尋找10公里內醫療與避難設施.py
+++ pages/5.尋找10公里內醫療與避難設施.py
@@ -11,3 +11,14 @@ font = FontProperties(fname = font_path)
 rcParams['font.family'] = font.get_name()
 
 st.set_page_config(layout="wide")
+
+m = leafmap.Map(center=[23, 120.3], zoom=10)
+folium.CircleMarker(
+    location=[-27.55, -48.8],
+    radius=10,
+    color="cornflowerblue",
+    fill=True,
+    fill_opacity=0.6,
+    opacity=1,
+    popup="{} pixels".format(radius),
+).add_to(m)

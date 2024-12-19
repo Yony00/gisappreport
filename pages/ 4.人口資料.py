ffef3368a371cadfd5b5a8e7db38c5163566e477
b6baa <tina200205106@gmail.com>
2024-12-19 11:51:59 +0800

    Update  4.人口資料.py

--- pages/ 4.人口資料.py
+++ pages/ 4.人口資料.py
@@ -26,7 +26,7 @@ st.markdown(markdown1)
 polygon = 'https://github.com/liuchia515/gisappreport/raw/refs/heads/main/data/%E9%84%89%E9%8E%AE%E5%B8%82%E5%8D%80%E7%95%8C/%E9%84%89(%E9%8E%AE%E3%80%81%E5%B8%82%E3%80%81%E5%8D%80)%E7%95%8C%E7%B7%9A(TWD97%E7%B6%93%E7%B7%AF%E5%BA%A6)1131028/TOWN_MOI_1131028.shp'
 taiwan = gpd.read_file(polygon)
 tainan = taiwan[taiwan['COUNTYNAME'] == '臺南市']
-pop = 'data/臺南市人口資料.csv'
+pop = 'data/臺南市人口資料pro.csv'
 tainan_pop = pd.read_csv(pop)
 
 tainan = tainan.rename(columns={'TOWNNAME': '行政區'})
